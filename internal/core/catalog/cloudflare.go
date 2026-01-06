a689a5c82741f871254455667b5cf70c301f43d0
n0paleon <muhammadnaufalalfattah@gmail.com>
2026-01-07 02:49:27 +0700

    edit: cloudflare catalog tags

--- internal/core/catalog/cloudflare.go
+++ internal/core/catalog/cloudflare.go
@@ -6,7 +6,7 @@ func CloudFlare() *domain.Technology {
 	return &domain.Technology{
 		Name:        "Cloudflare",
 		Version:     "",
-		Tags:        []string{"CDN", "proxy", "isp", "cyber security", "WAF"},
+		Tags:        []string{"CDN", "proxy", "isp", "cyber security", "WAF", "DNS"},
 		Description: "Cloudflare is a global cloud-based platform that provides security, performance, and reliability services for websites, apps, and networks.",
 		Link:        "https://cloudflare.com/",
 	}
